c24f7bdf68f073d8ad3b8d2812cbef4749314f98
sajeevvenugopan <sajeev.vs1991@yahoo.in>
2024-02-27 14:28:05 -0500

    first commit

---
+++ src/test/java/com/foodservice/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodservice.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
